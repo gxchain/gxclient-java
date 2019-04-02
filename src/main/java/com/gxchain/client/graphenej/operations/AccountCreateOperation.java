d8c796203afc9951b345d32142b8d412a7f96afe
superbigfu <512034786@qq.com>
2019-04-02 15:30:10 +0800

    fix transaction deserializer

--- src/main/java/com/gxchain/client/graphenej/operations/AccountCreateOperation.java
+++ src/main/java/com/gxchain/client/graphenej/operations/AccountCreateOperation.java
@@ -2,14 +2,13 @@ package com.gxchain.client.graphenej.operations;
 
 import com.google.common.primitives.Bytes;
 import com.google.common.primitives.UnsignedLong;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import com.gxchain.client.graphenej.Varint;
 import com.gxchain.client.graphenej.enums.OperationType;
 import com.gxchain.client.graphenej.objects.*;
 import lombok.Data;
+
+import java.lang.reflect.Type;
 
 /**
  * Class used to encapsulate operations related to the ACCOUNT_CREATE_OPERATION.
@@ -56,7 +55,35 @@ public class AccountCreateOperation extends BaseOperation {
         this.owner = owner;
         this.active = active;
         this.options = options;
-        extensions = new Extensions();
+        this.extensions = new Extensions();
+    }
+
+    /**
+     * Account create operation constructor.
+     *
+     * @param registrar       register account
+     * @param referrer
+     * @param owner           Owner authority to set. Can be null.
+     * @param active          Active authority to set. Can be null.
+     * @param options         Can be null
+     * @param fee             The fee to pay. Can be null.
+     * @param referrerPercent
+     * @param name            register name
+     */
+    public AccountCreateOperation(UserAccount registrar, UserAccount referrer,
+                                  Authority owner, Authority active,
+                                  AccountOptions options, AssetAmount fee,
+                                  int referrerPercent, String name) {
+        super(OperationType.ACCOUNT_CREATE_OPERATION);
+        this.fee = fee;
+        this.registrar = registrar;
+        this.referrer = referrer;
+        this.owner = owner;
+        this.active = active;
+        this.options = options;
+        this.extensions = new Extensions();
+        this.referrerPercent = referrerPercent;
+        this.name = name;
     }
 
     public AccountCreateOperation(UserAccount registrar, UserAccount referrer, Authority owner, Authority active, AccountOptions options) {
@@ -117,5 +144,46 @@ public class AccountCreateOperation extends BaseOperation {
         byte[] optionsBytes = options.toBytes();
         byte[] extensionBytes = extensions.toBytes();
         return Bytes.concat(feeBytes, registrarBytes, referrerBytes, referrerPercentBytes, new byte[]{(byte) this.name.length()}, nameBytes, ownerBytes, activeBytes, optionsBytes, extensionBytes);
+    }
+
+    public static class AccountCreateSerializer implements JsonSerializer<AccountCreateOperation> {
+
+        @Override
+        public JsonElement serialize(AccountCreateOperation accountCreate, Type type, JsonSerializationContext jsonSerializationContext) {
+            return accountCreate.toJsonObject();
+        }
+    }
+
+    public static class AccountCreateDeserializer implements JsonDeserializer<AccountCreateOperation> {
+
+        @Override
+        public AccountCreateOperation deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            if (json.isJsonArray()) {
+                JsonArray serializedTransfer = json.getAsJsonArray();
+                if (serializedTransfer.get(0).getAsInt() != OperationType.ACCOUNT_CREATE_OPERATION.getCode()) {
+                    return null;
+                } else {
+                    return context.deserialize(serializedTransfer.get(1), AccountCreateOperation.class);
+                }
+            } else {
+                JsonObject jsonObject = json.getAsJsonObject();
+
+                AssetAmount fee = context.deserialize(jsonObject.get(KEY_FEE), AssetAmount.class);
+                Authority owner = context.deserialize(jsonObject.get(KEY_OWNER), Authority.class);
+                Authority active = context.deserialize(jsonObject.get(KEY_ACTIVE), Authority.class);
+                AccountOptions options = context.deserialize(jsonObject.get(KEY_OPTIONS), AccountOptions.class);
+
+                UserAccount registrar = new UserAccount(jsonObject.get(KEY_REGISTRAR).getAsString());
+                UserAccount referrer = new UserAccount(jsonObject.get(KEY_REFERRER).getAsString());
+
+                int referrerPercent = jsonObject.get(KEY_REFERRER_PERCENT).getAsInt();
+                String name = jsonObject.get(KEY_NAME).getAsString();
+
+                AccountCreateOperation accountCreate = new AccountCreateOperation(
+                        registrar, referrer, owner, active, options, fee, referrerPercent, name);
+
+                return accountCreate;
+            }
+        }
     }
 }
